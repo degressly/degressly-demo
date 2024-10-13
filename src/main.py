467f2eb126c6dca91d6fd8610ae1698e360f4d43
Daniyaal Khan <me@daniyaalkhan.com>
2024-10-13 21:46:18 +0530

    Replay changes

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ port = sys.argv[1]
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/<route>")
+def hello_world(route):
     url = "https://postman-echo.com"
 
     payload = {"test": port, "constant": "constant", "will_regress": "no_regress", "time": time.time(), "price": 99.95}
 
     if (port == '8082'):
+        url = "https://google.com"
         payload["will_regress"] = "This field should be different to prove a point."
         payload["price"] = "99.94"
 
@@ -22,13 +23,16 @@ def hello_world():
         "x-degressly-trace-id": request.headers.get("x-degressly-trace-id")
     }
 
-    print(payload)
-    requests.request("POST", url, headers=headers, data=json.dumps(payload))
-
+    print(url, headers, payload)
+    resp = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+    print(resp)
+    
     return Response(getXml(payload["price"]), content_type="application/xml")
 
 
 def getXml(val):
+    # Candidate will respond with 99.94 for price 
+    # Primary and secondary will respond with 99.95
     return f"""<?xml version="1.0" encoding="UTF-8" ?>
  <root>
      <book category="web" cover="paperback">
